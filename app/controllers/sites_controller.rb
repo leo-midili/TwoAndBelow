326c66241ecd17868b03c04b9f8bd428cd6a7223
AMidili <tonywinslow@yahoo.com>
2017-12-30 14:56:34 -0500

    Change application back to the way leo had it.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class SitesController < ApplicationController
   def home
     @products = Product.all.for_sale
@@ -16,30 +15,3 @@ class SitesController < ApplicationController
   def coloring_pages
   end
 end
-=======
-class SitesController < ApplicationController
-  def home
-    @name = 'Leo Midili'
-  end
-
-  def about
-  end
-
-  def veggies
-  end
-
-  def dad
-  end
-
-  def coloring_pages
-  end
-
-
-  def leo
-  end
-
-  def gryphen
-  end
-
-end
->>>>>>> master
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Rails.application.routes.draw do
   devise_for :users, skip: [:registration]
 
@@ -8,6 +7,8 @@ Rails.application.routes.draw do
   get :dad, to: 'sites#dad'
   get :veggies, to: 'sites#veggies'
   get :coloring_pages, to: "sites#coloring_pages"
+  get :leo, to: 'sites#leo'
+  get :gryphen, to: 'sites#gryphen'
   get :admin, to: redirect('/users/sign_in')
 
   resources :products
@@ -15,17 +16,3 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-=======
-Rails.application.routes.draw do
-  root to: 'sites#home'
-
-  get :about, to: 'sites#about'
-  get :dad, to: 'sites#dad'
-  get :veggies, to: 'sites#veggies'
-  get :coloring_pages, to: "sites#coloring_pages"
-  get :leo, to: 'sites#leo'
-  get :gryphen, to: "sites#gryphen"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
->>>>>>> master
