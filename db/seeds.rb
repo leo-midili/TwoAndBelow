bfc1fff515deaa121d04969b98744f201d047747
Leo Midili <leomidili07@gmail.com>
2017-12-31 12:03:06 -0500

    Create seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.find_or_create_by(name: 'Veggies')
+
+Category.find_or_create_by(name: 'Comics')
